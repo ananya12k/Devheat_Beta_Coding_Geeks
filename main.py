2c8e134ba714dbfc7daaabc5143412c32380e021
Devansh <dhmakwana25@gmail.com>
2022-06-21 15:41:09 +0530

    Added login UI and some Pages.

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRectangleFlatButton
 
 from sms import sendSMS
 from screenNavigation import screen_helper
 
+class LoginScreen(Screen):
+    pass
+class MainScreen(Screen):
+    pass
 class MenuScreen(Screen):
     pass
-
 class ProfileScreen(Screen):
+    pass
+class FriendsScreen(Screen):
+    pass
+class MapScreen(Screen):
     pass
 
 
 sm = ScreenManager()
+sm.add_widget(LoginScreen(name='Login'))
+sm.add_widget(MainScreen(name='Main'))
 sm.add_widget(MenuScreen(name='Menu'))
 sm.add_widget(ProfileScreen(name='Profile'))
+sm.add_widget(FriendsScreen(name='Friends'))
+sm.add_widget(MapScreen(name='Map'))
 
 
 class Friends:
@@ -28,8 +38,8 @@ class SOSApp(MDApp):
 
     def build(self):
         screen = Builder.load_string(screen_helper)
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Yellow"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Amber"
 
         return screen
 
--- sms.py
+++ sms.py
@@ -1,8 +1,12 @@
 from twilio.rest import Client 
- 
-account_sid = 'AC069488126e2c0e0f9e065ce24d2f094b' 
-auth_token = 'f276f0be9438a2962dcc5843687bb7bf' 
-client = Client(account_sid, auth_token) 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+account_sid = os.getenv("SID")
+auth_token = os.getenv("TOKEN")
+client = Client(account_sid,auth_token) 
 
 def sendSMS(toNumber, userName):
     message = client.messages.create(
@@ -10,3 +14,5 @@ def sendSMS(toNumber, userName):
                             from_='+18125059453',       
                             body=userName + " is in danger."
                           ) 
+
+#sendSMS('+919106204370', "Devansh")
