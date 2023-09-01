8b85d9a98e692c667ad460cc322b9eaca67438a1
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2023-09-01 18:20:09 +0530

    Configurations of database and twilio changed

--- sms.py
+++ sms.py
@@ -11,6 +11,6 @@ client = Client(account_sid,auth_token)
 def sendSMS(userName, toNumber, msg):
     message = client.messages.create(
                             to=toNumber,
-                            from_='+18125059453',       
+                            from_='+12562738727',       
                             body=userName + msg
                           ) 
